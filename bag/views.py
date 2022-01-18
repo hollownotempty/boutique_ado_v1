d7bb2dbef417d0dfb654dea9ef05afb4f357ffcb
Tom Holohan <thomasholohan8@gmail.com>
2022-01-18 13:19:57 +0000

    Created checkout app and models

--- bag/views.py
+++ bag/views.py
@@ -14,7 +14,6 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the spcified product to the shopping bag """
 
-
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -27,13 +26,17 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'Added size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}.')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upper()} {product.name} to your bag.')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added size {size.upper()} {product.name} to your bag.')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}.')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag.')
@@ -88,4 +91,5 @@ def remove_from_bag(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item {e}')
         return HttpResponse(status=500)
